5bac8b60beb06435e6ced6815081443e35704858
jayshryyy <122954632+jayshryyy@users.noreply.github.com>
2023-01-19 09:49:23 +0530

    Add files via upload

---
+++ Second_Day_Training/Assignment/Multipleinheritance.java
@@ -0,0 +1,14 @@
+package com.firstdayjava;
+
+public class Multipleinheritance implements Travel, Eat{
+	
+	public void travel()
+	{
+		System.out.println("Animal is travelling");
+	}
+	public void eat()
+	{
+		System.out.println("Animal is eating");
+	}
+
+}
---
+++ Second_Day_Training/Assignment/Multipleinheritancetester.java
@@ -0,0 +1,14 @@
+package com.firstdayjava;
+
+public class Multipleinheritancetester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Multipleinheritance a = new Multipleinheritance();
+		a.eat();
+		a.travel();
+
+	}
+
+}
